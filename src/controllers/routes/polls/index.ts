014a92da18ad8ccbdbac9bd813a1355a39d68004
Dlurak <84224239+Dlurak@users.noreply.github.com>
2024-01-09 05:32:09 +0100

    Run biome

--- src/controllers/routes/polls/index.ts
+++ src/controllers/routes/polls/index.ts
@@ -7,49 +7,49 @@ import { HttpStatusCode } from "elysia-http-status-code";
 import { client } from "index";
 
 const pollRouter = new Elysia({ name: "pollRouter" })
-  .use(HttpStatusCode())
-  .use(isLoggedIn)
-  .get("/poll", ({ set, httpStatus }) => {
-    set.status = httpStatus.HTTP_501_NOT_IMPLEMENTED;
-    return { error: "not implemented yet" };
-  })
-  .post(
-    "/poll",
-    async ({ set, auth, httpStatus, body }) => {
-      const { isAuthorized } = auth;
-      if (!isAuthorized) {
-        set.status = httpStatus.HTTP_401_UNAUTHORIZED;
-        return UNAUTHORIZED;
-      }
+	.use(HttpStatusCode())
+	.use(isLoggedIn)
+	.get("/poll", ({ set, httpStatus }) => {
+		set.status = httpStatus.HTTP_501_NOT_IMPLEMENTED;
+		return { error: "not implemented yet" };
+	})
+	.post(
+		"/poll",
+		async ({ set, auth, httpStatus, body }) => {
+			const { isAuthorized } = auth;
+			if (!isAuthorized) {
+				set.status = httpStatus.HTTP_401_UNAUTHORIZED;
+				return UNAUTHORIZED;
+			}
 
-      const query = e.insert(e.Poll, { ...body });
-      const result = Try(
-        async () =>
-          await client.transaction(async (tx) => {
-            return await query.run(tx);
-          }),
-      ).catch(() => new Error());
+			const query = e.insert(e.Poll, { ...body });
+			const result = Try(
+				async () =>
+					await client.transaction(async (tx) => {
+						return await query.run(tx);
+					}),
+			).catch(() => new Error());
 
-      if (result instanceof Error) {
-        set.status = httpStatus.HTTP_500_INTERNAL_SERVER_ERROR;
-        return DATABASE_WRITE_FAILED;
-      }
+			if (result instanceof Error) {
+				set.status = httpStatus.HTTP_500_INTERNAL_SERVER_ERROR;
+				return DATABASE_WRITE_FAILED;
+			}
 
-      const { id } = await result;
+			const { id } = await result;
 
-      set.status = httpStatus.HTTP_201_CREATED;
-      return {
-        message: "Poll successfully created",
-        data: { id },
-      };
-    },
-    {
-      body: t.Object({
-        title: t.String({ minLength: 1 }),
-        description: t.Optional(t.String({ minLength: 1 })),
-        visibility: t.Union([t.Literal("PUBLIC"), t.Literal("PRIVATE")]),
-      }),
-    },
-  );
+			set.status = httpStatus.HTTP_201_CREATED;
+			return {
+				message: "Poll successfully created",
+				data: { id },
+			};
+		},
+		{
+			body: t.Object({
+				title: t.String({ minLength: 1 }),
+				description: t.Optional(t.String({ minLength: 1 })),
+				visibility: t.Union([t.Literal("PUBLIC"), t.Literal("PRIVATE")]),
+			}),
+		},
+	);
 
 export { pollRouter };
